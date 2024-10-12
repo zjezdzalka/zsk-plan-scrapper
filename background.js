9b290273d9564d97fabab36d8c6d00199774ba99
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-10-12 17:29:31 +0200

    working api

--- background.js
+++ background.js
@@ -1,4 +1,14 @@
-import getPlans from './api/getPlan.js';
+async function getPlans(i){
+    let url = `http://thingproxy.freeboard.io/fetch/https://www.zsk.poznan.pl/plany_lekcji/2023plany/technikum/plany/o${i}.html)`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => console.log(err));
+}
+
 chrome.runtime.onMessage.addListener(data =>{
     console.log(data);
     if(data.name == 'get-plan-data'){
