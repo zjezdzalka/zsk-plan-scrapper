e33b116bc86582859d4022d713ad9431797a4d64
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-10-13 13:53:22 +0200

    amount of classes + plan of each class

--- background.js
+++ background.js
@@ -1,17 +1,43 @@
+async function getMain(){
+    let url = `http://thingproxy.freeboard.io/fetch/https://www.zsk.poznan.pl/plany_lekcji/2023plany/technikum/lista.html)`;
+    console.log(url);
+    let data = await fetch(url)
+        .then(response => response.text())
+        .catch(err => console.log(err));
+        console.log("Recieved main site.");
+    return data;
+}
+
 async function getPlans(i){
     let url = `http://thingproxy.freeboard.io/fetch/https://www.zsk.poznan.pl/plany_lekcji/2023plany/technikum/plany/o${i}.html)`;
     console.log(url);
-    fetch(url)
+    let data = await fetch(url)
         .then(response => response.text())
-        .then(data => {
-            console.log(data);
-        })
         .catch(err => console.log(err));
+    chrome.storage.local.set({ [i]: data }).then(() => {
+        console.log("Value "+i+" is set.");
+    });
+    return data;
 }
 
-chrome.runtime.onMessage.addListener(data =>{
-    console.log(data);
-    if(data.name == 'get-plan-data'){
-        getPlans(1);
-    }
-})
+chrome.runtime.onConnect.addListener(function(port) {
+    console.assert(port.name === "get-plan-data");
+    port.onMessage.addListener(async function(msg) {
+        if (msg.request === "run-data"){
+            let text="Successful.";
+            for(let i = 1; i <= 32; i++){
+                await getPlans(i);
+            }
+            
+            port.postMessage({name:"values", text: text});
+        }
+        else if (msg.request === "run"){
+            let value = await getMain();
+            
+            port.postMessage({name:"amount", text: value});
+        }
+        else{
+            port.postMessage({text: "Error."});
+        }
+    });
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,60 @@
 var input = document.getElementById("f17_start_func");
 if (input) {
-    input.addEventListener("click", function() {
+    input.addEventListener("click", async function() {
         console.log("Click");
-        try{
-            chrome.runtime.sendMessage({name: "get-plan-data"});
-        }
-        catch(e){
+        try {
+            var port = chrome.runtime.connect({name: "get-plan-data"});
+            port.postMessage({request: "run"});
+            port.onMessage.addListener(function(msg) {
+                if (msg.name == "amount") {
+                    if (msg.text !== null) {
+                        console.log("Message text is not null:", msg.text);
+
+                        var parser = new DOMParser();
+                        var doc = parser.parseFromString(msg.text, 'text/html');
+                        
+                        var container = doc.querySelectorAll('#oddzialy>.el');
+                        var amount = container.length;
+
+                        if (container) {
+                            chrome.storage.local.set({ amount: amount }).then(() => {
+                                console.log("Amount is set.");
+                            });
+                        }
+                    }
+                    port.postMessage({request: "run-data"});
+                }
+                else if (msg.name == "amount") {
+                    if (msg.text !== null) {
+                        console.log("Message text is not null:", msg.text);
+                    }
+                    chrome.storage.local.get((result) => {
+                        for(let i = 1; i < result["amount"]; i++){
+                            console.log(result[i]);
+                        }
+                        console.log(result);
+                    });
+                }
+                else{
+                    console.log("Error. Received message doesn't exist.");
+                }
+            });
+        } catch (e) {
             console.log(e);
         }
+    });
+}
+
+var clear = document.getElementById("f17_clear_info_func");
+if (clear) {
+    clear.addEventListener("click", async function() {
+        chrome.storage.local.clear(function() {
+            var error = chrome.runtime.lastError;
+            if (error) {
+                console.error(error);
+            }
+            // do something more
+        });
+        chrome.storage.sync.clear();
     });
 }
